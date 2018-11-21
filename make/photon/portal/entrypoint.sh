8a2158a0b92494d0866ab2e992d0b22169f6f214
Meina Zhou <meinaz@vmware.com>
2018-11-21 15:13:32 +0800

    add -y
    Signed-off-by: Meina Zhou <meinaz@vmware.com>

--- make/photon/portal/entrypoint.sh
+++ make/photon/portal/entrypoint.sh
@@ -5,6 +5,19 @@ cd /build_dir
 cp -r /portal_src/* .
 ls -la
 
+# Update
+apt-get update
+apt-get install -y python 
+apt-get install -y python-pip
+apt-get install -y python-dev
+apt-get install -y libyaml-dev
+ 
+# Install app dependencies
+pip install --upgrade pip
+pip install --user PyYAML
+
+python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' swagger.yaml swagger.json
+
 cat ./package.json
 npm install
 
@@ -15,15 +28,5 @@ npm run link_lib
 ## Build production
 npm run release
 
-# Update
-apt-get update
-apt-get install -y python 
-apt-get install -y python-pip
-apt-get install python-dev
-apt-get install libyaml-dev
- 
-# Install app dependencies
-pip install --upgrade pip
-pip install --user PyYAML
 
-python -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, indent=4)' swagger.yaml swagger.json
+
